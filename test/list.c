ce672ee726072f654b9edd9953e85a7ca5c1b70f
David Graham <david.malcom.graham@gmail.com>
2014-05-20 19:48:14 -0600

    Add create tests.

--- test/list.c
+++ test/list.c
@@ -3,14 +3,30 @@
 
 #include "list.h"
 
-int main(int argc char *argv[]) {
+void assert(bool success, const char *message) {
+    if (!success) {
+        fprintf(stderr, "\t[list] failed: %s\n", message);
+        exit(1);
+    }
+}
+
+void test_create() {
+    struct list *list = list_create();
+    assert(list->first == NULL, "first pointer is null");
+    assert(list->last == NULL, "last pointer is null");
+    assert(list->length == 0, "list is empty");
+    list_destroy(list);
+}
+
+int main(int argc, char *argv[]) {
     struct list *list = list_create();
     if (!list) {
         exit(1);
     }
 
-    list_destroy(list);
+    test_create();
 
+    list_destroy(list);
     return 0;
 }
 
