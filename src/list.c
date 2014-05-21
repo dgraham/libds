3d2fdd6caa7ae7df3b2d66047b869b86b470155c
David Graham <david.malcom.graham@gmail.com>
2014-05-20 19:10:20 -0600

    Resize node array.

--- src/list.c
+++ src/list.c
@@ -1,18 +1,41 @@
 #include "list.h"
 
+bool resize(struct list *this, size_t capacity);
+
 struct list *list_create() {
     struct list *this = calloc(1, sizeof(struct list));
-    if (!list) {
-        return NULL:
+    if (!this) {
+        return NULL;
     }
 
     this->nodes = NULL;
+    this->length = 0;
+    this->capacity = 0;
+
+    if (!resize(this, 16)) {
+        list_destroy(this);
+        return NULL;
+    }
 
     return this;
 }
 
 void list_destroy(struct list *this) {
+    this->length = 0;
+    this->capacity = 0;
     free(this->nodes);
     free(this);
+}
+
+bool resize(struct list *this, size_t capacity) {
+    void **nodes = realloc(this->nodes, capacity * sizeof(void *));
+    if (!nodes) {
+        return false;
+    }
+
+    this->nodes = nodes;
+    this->capacity = capacity;
+
+    return true;
 }
 
--- src/list.h
+++ src/list.h
@@ -1,8 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+
 struct list {
     void **nodes;
+    size_t length;
+    size_t capacity;
 };
 
 struct list *list_create();
