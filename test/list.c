96ee3f0efb1cf827c9782938553b5b6969f5ff77
David Graham <david.malcom.graham@gmail.com>
2014-05-20 19:55:28 -0600

    Add push tests.

--- test/list.c
+++ test/list.c
@@ -12,9 +12,24 @@ void assert(bool success, const char *message) {
 
 void test_create() {
     struct list *list = list_create();
+
     assert(list->first == NULL, "first pointer is null");
     assert(list->last == NULL, "last pointer is null");
     assert(list->length == 0, "list is empty");
+
+    list_destroy(list);
+}
+
+void test_push() {
+    struct list *list = list_create();
+
+    char *item = "test";
+    assert(list_push(list, item), "item accepted");
+    assert(list->first != NULL, "first pointer is set");
+    assert(list->last == list->first, "last pointer equals first");
+    assert(list->length == 1, "length is incremented");
+    assert(list->first->value == item, "stored item value");
+
     list_destroy(list);
 }
 
@@ -25,6 +40,7 @@ int main(int argc, char *argv[]) {
     }
 
     test_create();
+    test_push();
 
     list_destroy(list);
     return 0;
